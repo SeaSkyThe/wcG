7f5b81a2d9287882974809c211f315ff4f9544b8
Marcelo Eduardo <marceloer2011@gmail.com>
2024-06-21 12:23:31 -0300

    feat: adding count lines (-l) flag

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"io"
 	"os"
@@ -12,15 +13,38 @@ func count_bytes(file io.Reader) int {
 	return len(byts)
 }
 
+func count_lines(file io.Reader) int {
+	buf := make([]byte, 32*1024)
+	count := 0
+	lineSep := []byte{'\n'}
+
+	for {
+		c, err := file.Read(buf)
+		count += bytes.Count(buf[:c], lineSep)
+
+		switch {
+		case err == io.EOF:
+			return count
+
+		case err != nil:
+			fmt.Errorf("Error when counting lines: %q", err)
+			return 0
+		}
+	}
+}
+
 func count_words(line string) int {
 	words_slice := strings.Split(line, " ")
 	return len(words_slice)
 }
 
-func ProcessFlags(flag string, file io.Reader) int {
+func ProcessFlags(flag string, file *os.File) int {
+    file.Seek(0, io.SeekStart)
 	count := 0
 	if flag == "-c" {
 		count = count_bytes(file)
+	} else if flag == "-l" {
+		count = count_lines(file)
 	}
 	return count
 }
@@ -33,7 +57,7 @@ func ReadFile(filename string) (*os.File, error) {
 		println("Usage: wcg -flag(optional) <filename>")
 		println("Available flags: \n -c: count the number of bytes \n -l: count the number of lines \n -w: count the number of words \n -m: count the number of characters \n no flags: will output the equivalent of using -c, -l and -w flags. ")
 	}
-    return file, err
+	return file, err
 }
 
 func main() {
@@ -44,7 +68,7 @@ func main() {
 		return
 	}
 	filename := args[len(args)-1]
-    file, _ := ReadFile(filename)
+	file, _ := ReadFile(filename)
 
 	count := ProcessFlags(args[1], file)
 	output := fmt.Sprintf("%d %s", count, filename)
